a2847f814892dd470c0b7daf66d383d995f3d64b
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-12-22 09:00:39 +0900

    テーブル並び替え追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,12 @@
 class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
     @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
     @boards = @boards.page(params[:page]).order('created_at DESC')
+
+    @boards_order = Board.all.order(sort_column + ' ' + sort_direction)
   end
 
   def new
@@ -50,5 +53,13 @@ class BoardsController < ApplicationController
 
   def set_target_board
     @board = Board.find(params[:id])
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def sort_column
+    Board.column_names.include?(params[:sort]) ? params[:sort] : "name"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def sortable(name, title = nil)
+    title ||= name.titleize
+    css_class = (name == sort_column) ? "current #{sort_direction}" : nil
+    direction = (name == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    link_to title, {:sort => name, :direction => direction}, {:class => css_class}
+  end
 end
