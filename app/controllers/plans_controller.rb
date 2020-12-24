b83ae11c1503abd232805d316630bb6de8ccf6c6
ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-12-24 17:19:09 +0900

    カレンダー機能の編集

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,9 +1,16 @@
 class PlansController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def index
     @plans = Plan.all
     # モーダル使用
     @plan = Plan.new
+
+    @boards_order = Board.all
+
+    search_date = '2020-12-24'
+    @todays_booking = Board.where(start_time: search_date.in_time_zone.all_day)
+
   end
 
   def new
@@ -42,5 +49,21 @@ class PlansController < ApplicationController
 
   def plan_params
     params.require(:plan).permit(:title, :content, :start_time)
+  end
+
+  def board_params
+    params.require(:board).permit(:name, :furigana, :age, :address, :tel, :email, :title, :body, :start_time, tag_ids: []).merge(user_id: current_user.id)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def sort_column
+    Board.column_names.include?(params[:sort]) ? params[:sort] : "name"
   end
 end
